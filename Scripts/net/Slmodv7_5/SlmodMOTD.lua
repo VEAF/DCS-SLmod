8f6e1e0bc8952a856e413fb812be07265d92c984
David Pierron <dpierron@gmail.com>
2021-02-17 12:00:09 +0100

    added the custom_MOTD_only option

--- Scripts/net/Slmodv7_5/SlmodMOTD.lua
+++ Scripts/net/Slmodv7_5/SlmodMOTD.lua
@@ -60,102 +60,104 @@ if slmod.config.MOTD_enabled then
 				titleTbl[#titleTbl + 1] = slmod.config.custom_MOTD
 				--title = slmod.config.custom_MOTD  -- use custom first line
 			end
-			titleTbl[#titleTbl + 1] = '\nThis server is running Slmod version '
-			titleTbl[#titleTbl + 1] = slmod.mainVersion
-			titleTbl[#titleTbl + 1] = ', build '
-			titleTbl[#titleTbl + 1] = slmod.buildVersion
-			titleTbl[#titleTbl + 1] = '.  To see the Slmod help menu, say "-help" in chat.\n'
-			--title = title .. '\nThis server is running Slmod version ' .. slmod.mainVersion .. ', build ' .. slmod.buildVersion .. '.\n' .. 'To see the Slmod help menu, say "-help" in chat.\n'
-			
-			if slmod.config.enable_slmod_stats then
-				titleTbl[#titleTbl + 1] = 'Stats tracking is enabled; to view player stats, say "-stats" in chat.\n'
-				--title = title .. 'This server is tracking stats with SlmodStats. To view the SlmodStats menu, say "-stats" in chat.\n'
-			else
-				titleTbl[#titleTbl + 1] = 'Stats tracking is disabled; to view player stats, say "-stats" in chat.\n'
-				--title = title .. 'This server currently has stats tracking with SlmodStats disabled.  If you are an admin, you may re-enable it with the Admin menu.\n'
-			end
-			
-			if slmod.config.autoAdmin.autoBanEnabled or slmod.config.autoAdmin.autoKickEnabled then
-				local offCounter = 0  -- offense counter.
-				if slmod.config.autoAdmin.teamHit.enabled then
-					offCounter = offCounter + 1
+			if (not slmod.config.custom_MOTD_only) then  -- use ONLY the custom MOTD
+				titleTbl[#titleTbl + 1] = '\nThis server is running Slmod version '
+				titleTbl[#titleTbl + 1] = slmod.mainVersion
+				titleTbl[#titleTbl + 1] = ', build '
+				titleTbl[#titleTbl + 1] = slmod.buildVersion
+				titleTbl[#titleTbl + 1] = '.  To see the Slmod help menu, say "-help" in chat.\n'
+				--title = title .. '\nThis server is running Slmod version ' .. slmod.mainVersion .. ', build ' .. slmod.buildVersion .. '.\n' .. 'To see the Slmod help menu, say "-help" in chat.\n'
+				
+				if slmod.config.enable_slmod_stats then
+					titleTbl[#titleTbl + 1] = 'Stats tracking is enabled; to view player stats, say "-stats" in chat.\n'
+					--title = title .. 'This server is tracking stats with SlmodStats. To view the SlmodStats menu, say "-stats" in chat.\n'
+				else
+					titleTbl[#titleTbl + 1] = 'Stats tracking is disabled; to view player stats, say "-stats" in chat.\n'
+					--title = title .. 'This server currently has stats tracking with SlmodStats disabled.  If you are an admin, you may re-enable it with the Admin menu.\n'
 				end
-				if slmod.config.autoAdmin.teamKill.enabled then
-					offCounter = offCounter + 1
-				end
-				if slmod.config.autoAdmin.teamCollisionHit.enabled then
-					offCounter = offCounter + 1
-				end
-				if slmod.config.autoAdmin.teamCollisionKill.enabled then
-					offCounter = offCounter + 1
-				end
-				if offCounter ~= 0 then
-					titleTbl[#titleTbl + 1] = 'WARNING! '
-					--title = title .. 'WARNING! '
-					if slmod.config.autoAdmin.autoKickEnabled then
-						titleTbl[#titleTbl + 1] = 'Auto Kick '
-						--title = title .. 'Auto Kick '
-						if slmod.config.autoAdmin.autoBanEnabled then
-							titleTbl[#titleTbl + 1] = 'and '
-							--title = title .. 'and '
-						end
-					end
-					if slmod.config.autoAdmin.autoBanEnabled then
-						titleTbl[#titleTbl + 1] = 'Auto Ban '
-						--title = title .. 'Auto Ban '
-					end
-					if slmod.config.autoAdmin.autoBanEnabled and slmod.config.autoAdmin.autoKickEnabled then
-						titleTbl[#titleTbl + 1] = 'are enabled. Actionable offenses include: '
-						--title = title .. 'are enabled. Actionable offenses include: '
-					else
-						titleTbl[#titleTbl + 1] = 'is enabled. Actionable offenses include: '
-						--title = title .. 'is enabled. Actionable offenses include: '
-					end
+						
+				if slmod.config.autoAdmin.autoBanEnabled or slmod.config.autoAdmin.autoKickEnabled then
+					local offCounter = 0  -- offense counter.
 					if slmod.config.autoAdmin.teamHit.enabled then
-						titleTbl[#titleTbl + 1] = 'team-hit'
-						--title = title .. 'team-hit'
-						if slmod.config.autoAdmin.teamKill.enabled or slmod.config.autoAdmin.teamCollisionHit.enabled or slmod.config.autoAdmin.teamCollisionKill.enabled then
-							titleTbl[#titleTbl + 1] = ', '
-							--title = title .. ', '
-						end
+						offCounter = offCounter + 1
 					end
 					if slmod.config.autoAdmin.teamKill.enabled then
-						titleTbl[#titleTbl + 1] = 'team-kill'
-						--title = title .. 'team-kill'
-						if slmod.config.autoAdmin.teamCollisionHit.enabled or slmod.config.autoAdmin.teamCollisionKill.enabled then
-							titleTbl[#titleTbl + 1] = ', '
-							--title = title .. ', '
-						end
+						offCounter = offCounter + 1
 					end
 					if slmod.config.autoAdmin.teamCollisionHit.enabled then
-						titleTbl[#titleTbl + 1] = 'team-collision-hit'
-						--title = title .. 'team-collision-hit'
-						if slmod.config.autoAdmin.teamCollisionKill.enabled then
-							titleTbl[#titleTbl + 1] = ', '
-							--title = title .. ', '
-						end
+						offCounter = offCounter + 1
 					end
 					if slmod.config.autoAdmin.teamCollisionKill.enabled then
-						titleTbl[#titleTbl + 1] = 'team-collision-kill'
-						--title = title .. 'team-collision-kill'
+						offCounter = offCounter + 1
 					end
-					titleTbl[#titleTbl + 1] = '.'
-					--title = title .. '.\n'
+					if offCounter ~= 0 then
+						titleTbl[#titleTbl + 1] = 'WARNING! '
+						--title = title .. 'WARNING! '
+						if slmod.config.autoAdmin.autoKickEnabled then
+							titleTbl[#titleTbl + 1] = 'Auto Kick '
+							--title = title .. 'Auto Kick '
+							if slmod.config.autoAdmin.autoBanEnabled then
+								titleTbl[#titleTbl + 1] = 'and '
+								--title = title .. 'and '
+							end
+						end
+						if slmod.config.autoAdmin.autoBanEnabled then
+							titleTbl[#titleTbl + 1] = 'Auto Ban '
+							--title = title .. 'Auto Ban '
+						end
+						if slmod.config.autoAdmin.autoBanEnabled and slmod.config.autoAdmin.autoKickEnabled then
+							titleTbl[#titleTbl + 1] = 'are enabled. Actionable offenses include: '
+							--title = title .. 'are enabled. Actionable offenses include: '
+						else
+							titleTbl[#titleTbl + 1] = 'is enabled. Actionable offenses include: '
+							--title = title .. 'is enabled. Actionable offenses include: '
+						end
+						if slmod.config.autoAdmin.teamHit.enabled then
+							titleTbl[#titleTbl + 1] = 'team-hit'
+							--title = title .. 'team-hit'
+							if slmod.config.autoAdmin.teamKill.enabled or slmod.config.autoAdmin.teamCollisionHit.enabled or slmod.config.autoAdmin.teamCollisionKill.enabled then
+								titleTbl[#titleTbl + 1] = ', '
+								--title = title .. ', '
+							end
+						end
+						if slmod.config.autoAdmin.teamKill.enabled then
+							titleTbl[#titleTbl + 1] = 'team-kill'
+							--title = title .. 'team-kill'
+							if slmod.config.autoAdmin.teamCollisionHit.enabled or slmod.config.autoAdmin.teamCollisionKill.enabled then
+								titleTbl[#titleTbl + 1] = ', '
+								--title = title .. ', '
+							end
+						end
+						if slmod.config.autoAdmin.teamCollisionHit.enabled then
+							titleTbl[#titleTbl + 1] = 'team-collision-hit'
+							--title = title .. 'team-collision-hit'
+							if slmod.config.autoAdmin.teamCollisionKill.enabled then
+								titleTbl[#titleTbl + 1] = ', '
+								--title = title .. ', '
+							end
+						end
+						if slmod.config.autoAdmin.teamCollisionKill.enabled then
+							titleTbl[#titleTbl + 1] = 'team-collision-kill'
+							--title = title .. 'team-collision-kill'
+						end
+						titleTbl[#titleTbl + 1] = '.'
+						--title = title .. '.\n'
+					end
+					
 				end
-				
-			end
 			
-			if slmod.config.coord_converter then
-				titleTbl[#titleTbl + 1] = '\nSlmod Coordinate Converter is enabled. For instructions on how to use it, say "-conv" in chat.'
-				--title = title .. 'This server has the Slmod Coordinate Converter enabled. For instructions on how to use it, say "-conv" in chat.\n'
+				if slmod.config.coord_converter then
+					titleTbl[#titleTbl + 1] = '\nSlmod Coordinate Converter is enabled. For instructions on how to use it, say "-conv" in chat.'
+					--title = title .. 'This server has the Slmod Coordinate Converter enabled. For instructions on how to use it, say "-conv" in chat.\n'
+				end
+				if slmod.config.MOTD_show_POS_and_PTS then
+					titleTbl[#titleTbl + 1] = '\nSome missions may make use of the Parallel Tasking System. To see any tasks, say "-stl" in chat.'
+					--title = title .. 'Some missions may make use of the Parallel Tasking System. To see any tasks, say "-stl" in chat.\n'
+					titleTbl[#titleTbl + 1] = '\nSome missions may make use of the Parallel Options System. To see any options, say "-sol" in chat.'
+					--title = title .. 'Some missions may make use of the Parallel Options System. To see any options, say "-sol" in chat.'
+				end
 			end
-			if slmod.config.MOTD_show_POS_and_PTS then
-				titleTbl[#titleTbl + 1] = '\nSome missions may make use of the Parallel Tasking System. To see any tasks, say "-stl" in chat.'
-				--title = title .. 'Some missions may make use of the Parallel Tasking System. To see any tasks, say "-stl" in chat.\n'
-				titleTbl[#titleTbl + 1] = '\nSome missions may make use of the Parallel Options System. To see any options, say "-sol" in chat.'
-				--title = title .. 'Some missions may make use of the Parallel Options System. To see any options, say "-sol" in chat.'
-			end
-			
+
 			if slmod.config.admin_tools and SlmodAdminMenu and clientId and slmod.clientInScope(clientId, SlmodAdminMenu:getScope()) then
 				titleTbl[#titleTbl + 1] = '\nYou are registered as a server admin.  Say "-admin" in chat to access the Admin menu.'
 				--title =  title ..'You are registered as a server admin.  Say "-admin" in chat to access the Admin menu.'
